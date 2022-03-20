f8d081b355085546673495a97e983ba88f366a39
Zeyneb Esra Öztürk <75810064+zeynebesra@users.noreply.github.com>
2022-03-20 21:52:16 +0300

    models

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const pageRoute = require('./routes/pageRoute');
+const courseRoute = require('./routes/courseRoute');
 
 const app = express();
+
+//Connect DB
+mongoose
+  .connect('mongodb://localhost/smartedu-db', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    //useFindAndModify: false,
+    useCreateIndex: true,
+  })
+  .then(() => {
+    console.log('DB Connected Successfuly');
+  });
 
 //TEMPLATE ENGINE
 app.set('view engine', 'ejs');
@@ -11,6 +25,7 @@ app.use(express.static('public')); //static dosyaları
 
 //ROUTES
 app.use('/', pageRoute);
+app.use('/courses', courseRoute);
 
 const port = 3000;
 
