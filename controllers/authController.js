58c64106a26bcbe5992f7099d635b6ff46baa4c5
Zeyneb Esra Öztürk <75810064+zeynebesra@users.noreply.github.com>
2022-03-22 10:55:32 +0300

    middleware

--- controllers/authController.js
+++ controllers/authController.js
@@ -26,7 +26,7 @@ exports.loginUser = (req, res) => {
           if (same) {
             // USER SESSION
             req.session.userID = user._id;
-            res.status(200).redirect('/');
+            res.status(200).redirect('/users/dashboard');
           }
         });
       }
@@ -44,5 +44,15 @@ exports.loginUser = (req, res) => {
 exports.logoutUser = (req, res) => {
   req.session.destroy(() => {
     res.redirect('/');
+  });
+};
+
+//dashboard
+
+exports.getDashboardPage = async (req, res) => {
+  const user = await User.findOne({ _id: req.session.userID });
+  res.status(200).render('dashboard', {
+    page_name: 'dashboard',
+    user,
   });
 };
